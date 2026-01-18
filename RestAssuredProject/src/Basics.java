ba235d709214ba0533bdc7d96c15fca1d9d4f749
sumedhvartak <sumedh16sep@gmail.com>
2026-01-18 22:00:51 +0530

    E2E for Post->Put->Get

--- RestAssuredProject/src/Basics.java
+++ RestAssuredProject/src/Basics.java
@@ -9,17 +9,37 @@ public class Basics {
 	public static void main(String[] args) {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		String request = Payload.getData();
-		System.out.println("Request: "+ request);
 		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").
 		body(request).
 		when().post("/maps/api/place/add/json").
-//		then().log().all().header("Server","Apache/2.4.52 (Ubuntu)").body("scope",equalTo("APP")).assertThat().statusCode(200).extract().response().asString();
-		then().extract().response().asString();
-		System.out.println(response);
-		
+		then().log().all().header("Server","Apache/2.4.52 (Ubuntu)").body("scope",equalTo("APP")).assertThat().statusCode(200).extract().response().asString();
+			
 		JsonPath js = new JsonPath(response);
 		String placeId = js.getString("place_id");
-		System.out.println(placeId);
+		
+		
+		//Updated Address using Put HTTP Request
+		String AddressToBeUpdated = "800, Sahara Desert";
+		given().queryParam("key", "qaclick123").header("Content-Type","application/json").
+		body("{\r\n"
+				+ "\"place_id\":\"" + placeId + "\",\r\n"
+				+ "\"address\":\"" + AddressToBeUpdated +"\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}\r\n"
+				+ "").
+		when().put("/maps/api/place/update/json").
+		then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated")).extract().response().asString();
+		
+		
+		//Retrieving Updated Address using Get HTTP Request
+		String getResponse = given().log().all().queryParam("key", "qaclick123").queryParam("place_id", placeId).
+		when().get("/maps/api/place/get/json").
+		then().log().all().statusCode(200).body("address", equalTo(AddressToBeUpdated)).extract().response().asString();
+		JsonPath js1 = new JsonPath(getResponse);
+		String updatedAddress = js1.getString("address");
+		System.out.println("Updated Address for Get: " + updatedAddress);
+
+		
 
 	}
 
